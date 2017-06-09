fb879f5015e5f55eb0447862dafc609bcbcef168
Byron MS. DeLaMatre <bdelamatre@Moberly.local>
2017-06-09 08:50:15 -0500

    added debugging to powershell script

--- wdc_install.ps1
+++ wdc_install.ps1
@@ -22,21 +22,59 @@
 #
 #
 
+Write-Host "Checking for elevated priveleges..." -nonewline
+
 # Ask for elevated privelege if needed
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     Exit
 }
 
+Write-Host "[OK]"
+
 # From Choclately: https://chocolatey.org/install
 # Don't forget to ensure ExecutionPolicy above
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-# Install puppet using chocalately
-choco install puppet
 
-#install puppet windows module
-puppet module install puppetlabs/windows
+# Make sure that Chocolatey installed properly
+if (Get-Command "choco" -errorAction SilentlyContinue)
+{
 
-#install puppet vcssrepo module
-puppet module install puppetlabs/vcsrepo
+	# Install puppet using chocalately
+	choco install puppet --yes
+
+	if (Get-Command $cmdName -errorAction SilentlyContinue)
+	{
+
+		#install puppet windows module
+		puppet module install puppetlabs/windows
+
+		#install puppet vcssrepo module
+		puppet module install puppetlabs/vcsrepo
+
+		Write-Host "All dependencies for WDC have been installed."
+		Write-Host "You can now apply WDC Puppet manifests using the following command:"
+		Write-Host ""
+		Write-Host "    puppet apply my_manifest_name.pp"
+		Write-Host ""
+		Write-Host "Be sure to replace my_manifest_name with your script's name"
+
+	}
+	else
+	{
+		
+		Write-Host "Puppet failed to install properly"
+		
+		#open the chocolatey log
+		Invoke-Item "%PROGRAMDATA%\chocolatey\logs\chocolatey.log"
+	
+	}
+	
+}
+else
+{
+
+	Write-Host "Choclatey failed to install properly"
+
+}
